89a62a9f6220081438eb26f3422e16e62c48557a
hope <hope@hope.hope>
2023-04-28 14:55:22 +0200

    dokończono stronę

--- db.sql
+++ db.sql
@@ -1,10 +1,72 @@
-CREATE DATABASE portflolio;
-USE portflolio;   
-    
-    CREATE TABLE realizacje (
-	id INT(11) NOT NULL AUTO_INCREMENT,
-	tytul VARCHAR(255) NOT NULL,
-	opis TEXT NOT NULL,
-	obrazek VARCHAR(255) NOT NULL,
-	PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2023 at 02:45 PM
+-- Wersja serwera: 10.4.28-MariaDB
+-- Wersja PHP: 8.2.4
+CREATE DATABASE portfolio;
+USE portfolio;   
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `portfolio`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `o_mnie`
+--
+
+CREATE TABLE `o_mnie` (
+  `id` int(11) NOT NULL,
+  `tytul` varchar(255) NOT NULL,
+  `opis` text NOT NULL,
+  `obrazek` varchar(255) NOT NULL,
+  `tekst1` text NOT NULL,
+  `obrazek2` varchar(255) NOT NULL,
+  `obrazek3` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `o_mnie`
+--
+
+INSERT INTO `o_mnie` (`id`, `tytul`, `opis`, `obrazek`, `tekst1`, `obrazek2`, `obrazek3`) VALUES
+(2, 'Informacje o mnie:', 'Mam 18 lat i jestem uczniem ZST w Słubicach. Uczę się na kierunku technik informatyk.\r\nNie przeszkadza mi to jednak w interesowaniu się takimi rzeczami jak, między innymi, historia, prawo. W swoim wolnym czasie lubię słuchać muzyki, w tym najróżniejszych gatunków, takich jak rock, metal, czy chociażby muzyka klasyczna. Lubię także gotować, oraz czytać i grać. Pasjonuję się także numizmatyką, oraz ogólnym kolekcjonowaniem starych rzeczy.', 'https://cdn.discordapp.com/attachments/948364094703013891/1101477336093839460/20230428_135556.jpg', 'Poniżej przedstawię parę monet jakie posiadam w swojej kolekcji.', 'https://cdn.discordapp.com/attachments/948364094703013891/1101477336689422346/20230428_135540.jpg', 'https://cdn.discordapp.com/attachments/948364094703013891/1101477337201131531/20230428_135615.jpg');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `o_mnie`
+--
+ALTER TABLE `o_mnie`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `o_mnie`
+--
+ALTER TABLE `o_mnie`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- index.php
+++ index.php
@@ -4,94 +4,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Portfolio</title>
-    <!-- Łączymy się z plikami CSS Bootstrapa -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fyq8W2pa5mv+Qb5Q6UivzIebhnd0MjVhE1Q6Jr" crossorigin="anonymous">
-    <!-- Dołączamy swój plik CSS -->
+    <!-- Plik CSS -->
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- Tworzymy header strony -->
-    <header class="bg-dark text-white">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6">
-                    <h1>Moje portfolio</h1>
-                </div>
-                <div class="col-md-6">
-                    <nav class="navbar navbar-expand-md navbar-dark">
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse" id="navbarNav">
-                            <ul class="navbar-nav ml-auto">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">O mnie</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Realizacje</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Kontakt</a>
-                                </li>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
-            </div>
+    <!-- Header -->
+    <header class="head">
+        <div>
+            <h1>Portfolio Aleksandra Ostrowskiego</h1>
         </div>
     </header>
-    <!-- Tworzymy sekcję z realizacjami -->
-    <section class="realizacje">
-        <div class="container">
-            <h2 class="text-center mb-5">Moje realizacje</h2>
-            <div class="row">
-                <?php
-                // Dane do połączenia z bazą danych
-                $servername = "localhost";
-                $username = "root";
-                $password = "";
-                $dbname = "portflolio";
+    <!-- Sekcja z informacjami o mnie -->
+    <section class="o_mnie">
+    <div>
+        <h2>O mnie</h2>
+        <?php
+        // Dane do połączenia z bazą danych
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "portfolio";
 
-                // Tworzymy połączenie z bazą danych
-                $conn = new mysqli($servername, $username, $password, $dbname);
+        // Połączenie z bazą
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-                // Sprawdzamy, czy udało się połączyć z bazą danych
-                if ($conn->connect_error) {
-                    die("Nie udało się połączyć z bazą danych: " . $conn->connect_error);
-                }
+        // Test połączenia z bazą danych
+        if ($conn->connect_error) {
+            die("Nie udało się połączyć z bazą danych: " . $conn->connect_error);
+        }
 
-                // Zapytanie do bazy danych o wszystkie realizacje
-                $sql = "SELECT * FROM realizacje";
-                $result = $conn->query($sql);
+        // Zapytanie do bazy danych o dane
+        $sql = "SELECT * FROM o_mnie";
+        $result = $conn->query($sql);
 
-                // Wyświetlamy realizacje na stronie
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo '<div class="col-md-4">';
-                        echo '<div class="card">';
-                        echo '<img class="card-img-top" src="' . $row["zdjecie"] . '" alt="' . $row["tytul"] . '">';
-                        echo '<div class="card-body">';
-                        echo '<h5 class="card-title">' . $row["tytul"] . '</h5>';
-                        echo '<p class="card-text">' . $row["opis"] . '</p>';
-                        echo '</div>';
-                        echo '</div>';
-                        echo '</div>';
-                    }
-                } else {
-                    echo "Brak realizacji do wyświetlenia";
-                }
-    
-                // Zamykamy połączenie z bazą danych
-                $conn->close();
-                ?>
-            </div>
-        </div>
-    </section>
-    
-    <!-- Łączymy się z plikami JavaScript Bootstrapa -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    </body>
+        // Wyświetlenie z bazy danych tekstu do opisu
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                echo '<div>';
+                echo '<h5 class="tytul">' . $row["tytul"] . '</h5>';
+                echo '</div>';
+                echo '<div>';
+                echo '<p class="opis">' . $row["opis"] . '</p>';
+                echo '</div>';
+                echo '<div>';
+                echo '<p class="tekst1">' . $row["tekst1"] . '</p>';
+                echo '</div>';
+                echo '<div class="row">';
+                echo '<div class="column">';
+                echo '<img class="obrazek1" src="' . $row["obrazek"] . '" alt="">';   
+                echo '</div>';
+                echo '<div class="column">';
+                echo '<img class="obrazek2" src="' . $row["obrazek2"] . '" alt="">';
+                echo '</div>';
+                echo '<div class="column">';
+                echo '<img class="obrazek3" src="' . $row["obrazek3"] . '" alt="">';
+                echo '</div>';
+            }
+        } else {
+            echo "Brak danych do wyświetlenia";
+        }
+
+        // Zamykamy połączenie z bazą danych
+        $conn->close();
+        ?>
+</section>
+</body>
 </html>    
                        
